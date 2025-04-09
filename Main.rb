9fa68ae9f73b9948e1719a644135c5129d35b293
tanvisoni2001 <tanvisony181@gmail.com>
2025-04-09 19:17:29 +0530

    Add Test cases for add functionality

--- Main.rb
+++ Main.rb
@@ -10,4 +10,3 @@ class Calculator
 	end
 end
 
-
---
+++ calculator_spec.rb
@@ -0,0 +1,27 @@
+# require 'rails_helper'
+require 'rspec'
+require './Main.rb'
+
+describe Calculator do 
+  subject(:calculator){Calculator.new}
+
+  describe '#Add' do 
+    context 'when the input is empty or 0' do
+      it 'output will be 0' do
+        expect(calculator.add("")).to eq(0) 
+      end 
+    end
+
+    context 'when the input string contains single number' do 
+      it 'returns the given number' do 
+        expect(calculator.add("1")).to eq(1)
+      end
+    end
+
+    context 'when the input string contains more than single number' do 
+      it 'return the sum of all numbers' do 
+        expect(calculator.add("1,3,5")).to eq(9)
+      end
+    end  
+  end
+end
